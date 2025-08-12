c35a03df7f6e95dd9cdf2c574a70023031de911f
Andrei <Andrei.NovikovS@infowatch.com>
2025-08-12 16:26:11 +0300

    косметические изменения в event_detail

--- rat_notebook/results/forms.py
+++ rat_notebook/results/forms.py
@@ -1,9 +1,6 @@
-# results/forms.py
-
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Athlete, DisciplineResult, Event, DisciplineType
-from django.contrib.auth.models import User
+from .models import Athlete, DisciplineResult, Event
 
 
 class LoginForm(forms.Form):
@@ -11,35 +8,7 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput, label="Пароль")
 
 
-# Параметры валидации по дисциплинам
-VALIDATION_RULES = {
-    'long_jump':    {'step': 10, 'min': 0,   'max': 800},
-    'wall_jump':    {'step': 10, 'min': 0,   'max': 500},
-    'high_jump':    {'step': 5,  'min': 0,   'max': 270},
-    'barrier_jump': {'step': 5,  'min': 0,   'max': 180},
-    'a_frame':      {'step': 1,  'min': 0,   'max': 80},
-    'treadmill':    {'step': 0.01, 'min': 0, 'max': 60},  # сек.
-}
-
-class AthleteForm(forms.ModelForm):
-    class Meta:
-        model = Athlete
-        fields = ['name', 'growth_category', 'is_champion']
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя спортсмена'}),
-            'growth_category': forms.Select(attrs={'class': 'form-control'}),
-            'is_champion': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
-        }
-        labels = {
-            'growth_category': 'Ростовая категория',
-            'is_champion': 'Чемпион',
-        }
-
-from django import forms
-from django.core.exceptions import ValidationError
-from .models import Athlete, DisciplineResult
-
-# Правила валидации для каждой дисциплины
+# ---- Правила валидации результатов ----
 VALIDATION_RULES = {
     'long_jump':    {'step': 10,   'min': 0,   'max': 800},
     'wall_jump':    {'step': 10,   'min': 0,   'max': 500},
@@ -49,41 +18,49 @@ VALIDATION_RULES = {
     'treadmill':    {'step': 0.01, 'min': 0,   'max': 60},  # секунды с сотыми
 }
 
-# class DisciplineResultForm(forms.ModelForm):
-#     class Meta:
-#         model = DisciplineResult
-#         fields = ['athlete', 'discipline', 'result']
-#         widgets = {
-#             'athlete':    forms.Select(attrs={'class': 'form-select'}),
-#             'discipline': forms.Select(attrs={'class': 'form-select'}),
-#             'result':     forms.NumberInput(attrs={
-#                 'class': 'form-control', 'step': '0.01', 'placeholder': 'Результат'
-#             }),
-#         }
-#
-#     def __init__(self, *args, event=None, **kwargs):
-#         super().__init__(*args, **kwargs)
-#
-#         if event is not None:
-#             # 1) только спортсмены этого события
-#             self.fields['athlete'].queryset = event.athletes.order_by('name')
-#             # 2) только дисциплины, добавленные в это событие
-#             self.fields['discipline'].queryset = event.disciplines.all()
-#
-#         # подпись спортсмена с эмодзи для чемпионов
-#         def athlete_label(obj):
-#             cup = ' 🏆' if obj.is_champion else ''
-#             return f"{obj.name} ({obj.growth_category}){cup}"
-#         self.fields['athlete'].label_from_instance = athlete_label
 
+# ---- Спортсмен ----
+class AthleteForm(forms.ModelForm):
+    class Meta:
+        model = Athlete
+        fields = ['name', 'growth_category', 'is_champion']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Имя спортсмена'
+            }),
+            # делаем таким же селектом, как у athlete в форме результатов
+            'growth_category': forms.Select(attrs={'class': 'form-select'}),
+            'is_champion': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+        labels = {
+            'growth_category': 'Ростовая категория',
+            'is_champion': 'Чемпион',
+        }
+
+
+# ---- Результат дисциплины ----
 class DisciplineResultForm(forms.ModelForm):
     class Meta:
         model = DisciplineResult
         fields = ['athlete', 'discipline', 'result']
         widgets = {
-            'athlete':    forms.Select(attrs={'class': 'form-select'}),
-            'discipline': forms.Select(attrs={'class': 'form-select'}),
-            'result':     forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01', 'placeholder': 'Результат'}),
+            'athlete': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+            'discipline': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+            'result': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'step': 1,  # изменили шаг на 1
+                'placeholder': 'Результат'
+            }),
+        }
+        labels = {
+            'athlete': 'Выберите спортсмена',
+            'discipline': 'Выберите дисциплину',
+            'result': 'Результат'
         }
 
     def __init__(self, *args, event=None, **kwargs):
@@ -119,16 +96,15 @@ class DisciplineResultForm(forms.ModelForm):
                     )
         return result
 
+
+# ---- Событие ----
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['name', 'date', 'disciplines']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'date': forms.DateInput(attrs={
-                'type': 'date', 'class': 'form-control'
-            }),
-            # <-- заменили SelectMultiple на CheckboxSelectMultiple
+            'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'disciplines': forms.CheckboxSelectMultiple(),
         }
         labels = {
